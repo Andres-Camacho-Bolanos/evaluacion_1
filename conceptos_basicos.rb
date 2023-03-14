92ed51cfc406f50da3a24421e3277f2fb50ab3c2
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-03-14 02:47:03 -0600

    Create conceptos_basicos.rb

---
+++ conceptos_basicos.rb
@@ -0,0 +1,34 @@
+#Formato de comentarios (Una sola línea)
+=begin
+Hola
+Mundo (Multilínea)
+=end
+
+#Asignación de variables
+numero = 10
+numero2 = 20
+resultado = numero + numero2
+
+#Otro tipo de string (Cadena de caracteres)
+puts ':)'
+
+=begin
+Operadores y tipos de números en Ruby
+Aquí solo existen 2 tipos de números: Enteros y flotantes.
+=end
+puts 2.5 / 8.0
+puts 3 + 7
+puts "El resultado de la suma es:"
+puts resultado
+    
+#Concatenar cadenas
+puts "Ruby\s" + "rules"
+
+#Repetir instrucciones
+puts "Te amo Estefani <3\s" * 2
+
+#Cambio de valores en las variables
+puts numero * 5
+numero = 7
+puts "El nuevo valor del número es:"
+puts numero
