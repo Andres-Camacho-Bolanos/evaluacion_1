848fb35312ab57cf82bad4ad781be0dc71f0e0f0
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-03-15 01:57:29 -0600

    Update tabla_multiplicacion_2.rb

--- tabla_multiplicacion_2.rb
+++ tabla_multiplicacion_2.rb
@@ -1,8 +1,13 @@
-numero = 1
+numero = 0
 
-while numero <= 10
+loop do 
     
     puts "#{numero} * 2 = #{numero * 2}"
     numero = numero + 1
-   
+    
+    if numero > 10
+    break 
+        
+    end
+    
 end
